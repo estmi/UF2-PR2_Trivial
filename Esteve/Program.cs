ffae03f869d7247c29eabeb7c0ba6b1e7f033bec
estmi <estmiqgui@outlook.es>
2021-02-10 13:04:42 +0100

    Programa acabat amb possibilitat de millora. Falta fer les preguntes i afegir els StreamReaders

--- Esteve/Program.cs
+++ Esteve/Program.cs
@@ -2,13 +2,33 @@
 using System.Collections.Generic;
 using System.Linq;
 using Programacio;
+using System.IO;
 
 namespace Esteve
 {
     public class Esteve
     {
+        public static Tematica FarmingSim;
+        public static Tematica Dates;
+        public static Tematica Mates;
         public static Random _random = new Random();
         public static string[] abc =  { "a)","b)","c)","d)","e)","f)","g)","h)","i)","j)","k)","l)","m)","n)"};
+        public struct Tematica
+        {
+            public Correccio pregunta1;
+            public Correccio pregunta2;
+            public Correccio pregunta3;
+            public Correccio pregunta4;
+            public Correccio pregunta5;
+            public Tematica(Correccio c1,Correccio c2,Correccio c3,Correccio c4,Correccio c5)
+            {
+                this.pregunta1 = c1;
+                this.pregunta2 = c2;
+                this.pregunta3 = c3;
+                this.pregunta4 = c4;
+                this.pregunta5 = c5;
+            }
+        }
         public struct Correccio
         {
             public string pregunta;
@@ -23,26 +43,59 @@ namespace Esteve
         }
         public static void Main() 
         {
-            Correccio c,c2,c3,c4,c5;
-            c.pregunta = "És any de traspas?";
-            c.opcions = new string[3] { "2000", "2002", "2003" };
-            c.OpcioCorrecta = new bool[c.opcions.Length];
-            c = CompletarCorrecio(c, "EsAnyDeTraspas");
-            c2.pregunta = "Quin nombre és primer?";
-            c2.opcions =new string[4] { "3", "6","8","15" };
-            c2.OpcioCorrecta = new bool[c.opcions.Length];
-            c = CompletarCorrecio(c, "EsAnyDeTraspas");
-            c2 = CompletarCorrecio(c2, "EsPrimer");
+            StreamReader sr = new StreamReader("../../../Preguntes Farming.txt");
+            FarmingSim.pregunta1 = AfegirPregunta(sr);
+            sr.ReadLine();
+            FarmingSim.pregunta2 = AfegirPregunta(sr);
+            sr.ReadLine();
+            Console.WriteLine(FerPregunta(FarmingSim.pregunta1));
+            
+            
+
+        }
+        /// <summary>
+        /// Aquesta funcio fa una pregunta i retorna si s'ha fet be o no
+        /// </summary>
+        /// <param name="c">Aqui entrem la correccio de la pregunta</param>
+        /// <returns>1 punt correcte, 0 punts incorrecte</returns>
+        public static int FerPregunta(Correccio c)
+        {
+            int punt = 0;
+            int tecla;
             c = RandomizeOptions(c);
             MostrarOpcions(c);
-            MostrarOpcions(c2);
-            
+            tecla = (int)Console.ReadKey().Key - 65;
+            Console.WriteLine();
+            if (c.OpcioCorrecta[tecla])
+                punt ++;            
+            return punt;
+        }
+        public static Correccio AfegirPregunta(StreamReader sr)
+        {
+            Correccio c;
+            c.pregunta = sr.ReadLine();
+            c.opcions = sr.ReadLine().Split('|');
+            c.OpcioCorrecta = new bool[c.opcions.Length];
+            c.OpcioCorrecta[Convert.ToInt32(sr.ReadLine()) - 1] = true;
+            return c;
+        }
+        public static Correccio AfegirPregunta()
+        {
+            Correccio c;
+            Console.WriteLine("Pregunta a guardar:");
+            c.pregunta=Console.ReadLine();
+            Console.WriteLine("Possibles respostes a guardar, separades per Pipe [|]:");
+            c.opcions = Console.ReadLine().Split('|');
+            c.OpcioCorrecta = new bool[c.opcions.Length];
+            Console.WriteLine("Opcio correcta: ");
+            c.OpcioCorrecta[Convert.ToInt32(Console.ReadLine()) - 1] = true;
+            return c;
         }
         public static Correccio CompletarCorrecio(Correccio c, string s)
         {
             if (s == "EsAnyDeTranspas")
                 for (int i = 0; i < c.opcions.Length; i++)
-                    c.OpcioCorrecta[i] = FuncionsStandard.EsAnyDeTraspas(Convert.ToInt32(c.opcions[i]));
+                    c.OpcioCorrecta[i] = FuncionsStandard.AnyTraspas(Convert.ToInt32(c.opcions[i]));
             
             else if (s == "EsPrimer") 
                 for (int i = 0; i < c.opcions.Length; i++)
@@ -55,7 +108,8 @@ namespace Esteve
             Console.Write("{0}\n",ss.pregunta);
             for (int i = 0; i < ss.opcions.Length; i++)
             {
-                Console.WriteLine("{0}{1}",abc[i],ss.opcions[i]);
+                Console.WriteLine("{0} {1}",abc[i],ss.opcions[i]);
+                //Console.WriteLine("{0} {1}", abc[i], ss.OpcioCorrecta[i]);
             }
         }
         /// <summary>
