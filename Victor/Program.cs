dc3af69df36326a6389b448f87fc0c7afa599f1f
Victor <a2006264@institutmontilivi.cat>
2021-02-12 14:36:39 +0100

    Revisat

--- Victor/Program.cs
+++ Victor/Program.cs
@@ -8,24 +8,24 @@ namespace Victor
     {
         static void Main()
         {
-            Tematica FarmingSim;
+            Tematica Variable;
             string tema;
             tema = Console.ReadLine();
             StreamReader sr = new StreamReader(string.Format("../../../../Esteve/{0}.txt",tema));
             int punts = 0;
-            FarmingSim.nom = sr.ReadLine();
-            FarmingSim.pregunta1 = AfegirPregunta(sr);
+            Variable.nom = sr.ReadLine();
+            Variable.pregunta1 = AfegirPregunta(sr);
             sr.ReadLine();
-            FarmingSim.pregunta2 = AfegirPregunta(sr);
+            Variable.pregunta2 = AfegirPregunta(sr);
             sr.ReadLine();
-            punts = ComptadorPunts(FerPregunta(FarmingSim.pregunta1), punts);
-            punts = ComptadorPunts(FerPregunta(FarmingSim.pregunta2), punts);
+            punts = ComptadorPunts(FerPregunta(Variable.pregunta1), punts);
+            punts = ComptadorPunts(FerPregunta(Variable.pregunta2), punts);
         }
         public static int ComptadorPunts(bool b, int punts)
         {
             string s;
             if (b) s = string.Format("Correcte! la teva puntuació és de {0} punt/s", ++punts);
-            else s = string.Format("Correcte! la teva puntuació és de {0} punt/s", punts);
+            else s = string.Format("Incorrecte! la teva puntuació és de {0} punt/s", punts);
             Console.WriteLine(s);
             return punts;
         }
